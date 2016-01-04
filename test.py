10a596dfda923623bcc175d1946a0b7e8cb46396
perryjd <perryj89@gmail.com>
2016-01-04 15:02:33 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,5 @@
+print("This program calculates mpg.")
+miles_driven = float(input("Enter miles driven:"))
+gallons_used = float(input("Enter gallons used:"))
+mpg = miles_driven / gallons_used
+print("Miles per gallon:", mpg)
